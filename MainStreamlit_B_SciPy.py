07685fa43bf334df743e5d65b025ef6cd185721f
clairesoumokil07 <claire.mokil@gmail.com>
2024-10-26 00:17:50 +0900

    Add files via upload

---
+++ MainStreamlit_B_SciPy.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pickle
+import os
+
+model_directory - r'Dataset UTS_Gasal 2425.csv'
+
+model_path = os.path.join(model_directory, 'BestModel_CLF_RandomForest.pkl', 'BestModel_REG_Ridge.pkl')
